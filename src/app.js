2bbac1160f24c417c6b4efcb90b961e45f5218b9
Md. Maruf Rien <maruf14222@gmail.com>
2022-05-06 18:24:57 +0600

    refactor: clean up app and twitter file

--- src/app.js
+++ src/app.js
@@ -8,33 +8,3 @@ const {
   recordData,
   likeTweets,
 } = require('./twitter');
-
-//main function
-const main = async () => {
-  let filteredTweets = [];
-
-  while (filteredTweets.length < 49) {
-    const search = await getTweets();
-    const newTweets = await filterTweets(search);
-    filteredTweets = [...filteredTweets, ...newTweets];
-  }
-
-  if (filteredTweets.length > 50) {
-    const newTweets = filteredTweets.filter((tweet, i) => i < 50);
-    filteredTweets = [...newTweets];
-  }
-
-  likeTweets(filteredTweets);
-
-  // // send data to browser to display
-  // app.get('/', (req, res) => {
-  //   res.send(filteredTweets);
-  // });
-};
-
-setInterval(main, +process.env.INTERVAL);
-
-app.listen(process.env.PORT, () => {
-  //local
-  console.log('listening on port 3000');
-});
--- src/twitter.js
+++ src/twitter.js
@@ -10,93 +10,14 @@ const blackListWords = [
 ];
 
 // get tweets with selected query
-const getTweets = async () => {
-  try {
-    const search = await rwClient.v2.search(process.env.Q, {
-      'user.fields': 'name,description',
-      expansions: 'author_id',
-      max_results: 100,
-    });
-    console.log('searching for tweets' + Date.now());
-    return search;
-  } catch (err) {
-    console.log('the error is', err);
-  }
-};
+const getTweets = async () => {};
 
 // filter tweets,user,description for blacklisted words
-const filterTweets = async (searchData) => {
-  try {
-    // const tweets = searchData._realData.data;
-    const userInfo = searchData._realData.includes.users;
-    const tweets = searchData._realData.data;
-    const whiteList = [];
-    const tweetsToBeLiked = [];
+const filterTweets = async () => {};
 
-    userInfo.forEach((user) => {
-      const description = user.description;
-      const username = user.name;
-      const name = user.name;
-
-      //check if any of the strings contains blacklisted words
-      const descriptionCheck = blackListWords.some((word) =>
-        description.toLowerCase().includes(word)
-      );
-      const usernameCheck = blackListWords.some((word) =>
-        username.toLowerCase().includes(word)
-      );
-      const nameCheck = blackListWords.some((word) =>
-        name.toLowerCase().includes(word)
-      );
-
-      user.nameCheck = nameCheck;
-      user.usernameCheck = usernameCheck;
-      user.descriptionCheck = descriptionCheck;
-
-      if (!nameCheck && !usernameCheck && !descriptionCheck) {
-        whiteList.push(user);
-      }
-    });
-    tweets.forEach((tweet) => {
-      const existingTweets = whiteList.find(
-        (user) => user.id === tweet.author_id
-      );
-      if (existingTweets) {
-        tweetsToBeLiked.push(tweet);
-      }
-    });
-
-    return tweetsToBeLiked;
-  } catch (err) {
-    console.log('the error is', err);
-  }
-};
-
-// like tweets
-const likeTweets = async (tweets) => {
-  try {
-    //finding current user's id
-    const currentUser = await rwClient.v2.me().then((me) => {
-      return me.data.id;
-    });
-
-    tweets.forEach(async (tweet, i) => {
-      if (i < 49) {
-        await rwClient.v2.like(currentUser, tweet.id);
-        console.log('like tweet' + Date.now());
-      }
-    });
-  } catch (err) {
-    console.log('the error is', err);
-  }
-};
+const likeTweets = async () => {};
 
 // send statical data to a database or a spreed sheet
-const recordData = async () => {
-  try {
-  } catch (err) {
-    console.log('the error is', err);
-  }
-};
+const recordData = async () => {};
 
 module.exports = { getTweets, likeTweets, filterTweets, recordData };
