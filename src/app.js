4a6b5f1641c012bce8e9129a5e4251edae6f4cc3
Md. Maruf Rien <maruf14222@gmail.com>
2022-05-09 22:49:02 +0600

    feat: add function to run continuously

--- src/app.js
+++ src/app.js
@@ -15,30 +15,9 @@ const main = async () => {
     const searchResult = await getTweets();
     const selectedTweets = await filterTweets(searchResult);
     const operationResult = await likeTweets(selectedTweets);
-
-
-    //////////////
-    app.get("/", (req, res) => {
-      ;
-      res.send(operationResult)
-    })
-
-    /////////////////
-
-    // likeTweets
-    // const result = await likeTweets(dummyData.arrayOfTweets);
-    // if (result.success) {
-    //   console.log(result);
-    //   console.log("success fully liked selected tweets");
-    // } else {
-    //   console.log("failed to like selected tweets");
-    // }
-
+    console.log(operationResult);
 
   } catch (err) { console.log(err) }
 };
 
-main();
-
-
-app.listen(3000, () => { console.log("server in running"); })
+setInterval(main, 1000 * 60 * 5);
